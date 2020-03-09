5b26fa202d75451c1f12861ab69c77611d6b9c2c
SGmagicsam <38333794+SGmagicsam@users.noreply.github.com>
2020-03-09 10:53:49 -0600

    Undoing what I did, i'll have to rethink my strategy

--- portfolio/javascript/portfolio.js
+++ portfolio/javascript/portfolio.js
@@ -2,9 +2,8 @@ var app = new Vue({
 	el: "#projects",
 	data: {
 		projects: [
-			{content: {message}},
-			{content: "Project 2"}
+			{name: "Kirchoff Circuit Solver"},
+			{name: "Project 2"}
 		]
 	}
 })
-<script type = "text/javascript" src ="...is-it-prime/javascript/app.js"></script>
